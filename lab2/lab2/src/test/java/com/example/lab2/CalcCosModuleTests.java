b94ade348511deef3a1ef72ee0c16bb2a2172f5c
Alexander Shcherbakov <Neegroom@yandex.ru>
2024-05-02 23:36:13 +0300

    calcCos test

--- lab2/lab2/src/test/java/com/example/lab2/CalcCosModuleTests.java
+++ lab2/lab2/src/test/java/com/example/lab2/CalcCosModuleTests.java
@@ -1,6 +1,5 @@
 package com.example.lab2;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,19 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcCosModuleTests {
 
-    private BigDecimal calcSinMock(BigDecimal x){
-        return
-                switch (x.intValue()) {
-                    case 1 -> BigDecimal.valueOf(0.8414709848);
-                    case 2 -> BigDecimal.valueOf(0.90929742682);
-                    case -1 -> BigDecimal.valueOf(-0.8414709848);
-                    case -4 -> BigDecimal.valueOf(0.7568024953);
-                    default -> throw new IllegalArgumentException("this is mock. it is stupid");
-        };
-    }
-
     @Test
     public void calcCosTest(){
-        assertEquals(0.54030230586, calcCos(BigDecimal.valueOf(1), this::calcSinMock));
+        assertEquals(BigDecimal.ZERO, calcCos(BigDecimal.valueOf(1), (e) -> BigDecimal.ZERO));
     }
 }
