735166611e8e146879082b2bb544bb9b49e29587
Alexander Shcherbakov <Neegroom@yandex.ru>
2024-05-03 03:46:26 +0300

    first integration tests

--- lab2/lab2/src/test/java/integration/CosIntegrationTests.java
+++ lab2/lab2/src/test/java/integration/CosIntegrationTests.java
@@ -1,4 +1,22 @@
 package integration;
 
+import com.example.lab2.primitives.Cos;
+import com.example.lab2.primitives.Sin;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CosIntegrationTests {
+
+    @ParameterizedTest
+    @CsvSource({"0.0, 1.0", "1.5707963, 0.0", "3.1415926, -1", "-3.1415926, -1", "1.5707963, 0.0"})
+    public void calcCosTest(double x, double y){
+        Sin sin = new Sin();
+        Cos cos = new Cos(sin::calcSin);
+
+        assertEquals(y, cos.calcCos(BigDecimal.valueOf(x)).doubleValue(), 0.0001);
+    }
 }
--- lab2/lab2/src/test/java/integration/LogIntegrationTests.java
+++ lab2/lab2/src/test/java/integration/LogIntegrationTests.java
@@ -1,4 +1,22 @@
 package integration;
 
+import com.example.lab2.primitives.Ln;
+import com.example.lab2.primitives.Log;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LogIntegrationTests {
+
+    @ParameterizedTest
+    @CsvSource({""})
+    public void calcCosTest(double x, double y){
+        Ln ln = new Ln();
+        Log log = new Log(ln::calcLn);
+
+        assertEquals(y, log.calcLog(BigDecimal.valueOf(x), 2).doubleValue(), 0.0001);
+    }
 }
